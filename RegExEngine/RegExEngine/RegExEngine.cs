745cd446ba0879fe8b7d976d68dd92d64ee5976e
RkNjeru <35582018+RkNjeru@users.noreply.github.com>
2018-08-03 22:15:23 -0400

    Cleared Text Box

--- RegExEngine/RegExEngine/RegExEngine.cs
+++ RegExEngine/RegExEngine/RegExEngine.cs
@@ -27,6 +27,7 @@ namespace RegExEngine
             {
                 // not a valid input
                 MessageBox.Show("You sumbited an invalid Regular Expression");
+                textBox1.Text = null;
             }
             else
             {
